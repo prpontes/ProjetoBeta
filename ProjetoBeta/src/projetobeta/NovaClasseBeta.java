b1edb15e68137d477a0b3ab8067b75793aaa3a9f
Paulo Ricardo <prsilvapontes@gmail.com>
2018-04-01 23:08:50 -0300

    Geração dos getts e setts.

--- ProjetoBeta/src/projetobeta/NovaClasseBeta.java
+++ ProjetoBeta/src/projetobeta/NovaClasseBeta.java
@@ -11,6 +11,22 @@ package projetobeta;
  */
 public class NovaClasseBeta {
     
-    String nome;
-    int idade;
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
